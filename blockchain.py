538ebcf3be9032f1a2545c99597815803eea5f0d
Aditya Arora <aadityaaaaroraa@gmail.com>
2017-12-16 00:42:48 +0530

    Simple Proof of Work Algo implemented

--- blockchain.py
+++ blockchain.py
@@ -64,3 +64,20 @@ class Blockchain(object):
 
 
 
+    # For bitcoin its (PoW Algo) called as HashCash
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof=0
+        while self.valid_proof(last_proof,proof) is False:
+            proof+=1
+
+        return proof
+
+
