c7b2f721e0e7ad2d907c4d5ce99224a4924af1a3
Aditya Arora <aadityaaaaroraa@gmail.com>
2017-12-16 00:43:36 +0530

    Validating PoW

--- blockchain.py
+++ blockchain.py
@@ -81,3 +81,17 @@ class Blockchain(object):
         return proof
 
 
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+        # Difficulty of Algorithm can be adjusted by modifying no. of leading zeros which are 4 in this case
