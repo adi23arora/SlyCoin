0f4e9a48e76b105a3e9e8f7c71f97056643029bf
Aditya Arora <aadityaaaaroraa@gmail.com>
2017-12-16 00:40:57 +0530

    Blockchain class added with basic methods

---
+++ blockchain.py
@@ -0,0 +1,66 @@
+	######################################### 
+	#										#
+	# Implementation of Basic Blockchain	#
+	#										#
+	#########################################
+
+import hashlib
+import json
+from time import time
+
+
+
+class Blockchain(object):
+
+
+	def __init__(self):
+        
+        self.chain = []
+        self.current_transactions = []
+
+
+        # Creating the Genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount
+        })
+
+        return self.last_block['index']+1
+
+
+    
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        
+        :param block: <dict> Block
+        :return: <str> string after hashing
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+
+
